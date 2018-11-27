8ecd9081daa4674c8a2fb86a8551bbf3cb7284f6
Sergey Cheperis <czz@czz.name>
2018-11-27 15:37:03 +0300

    Fix

--- android/src/main/java/com/facebook/reactnative/androidsdk/FBGraphRequestModule.java
+++ android/src/main/java/com/facebook/reactnative/androidsdk/FBGraphRequestModule.java
@@ -160,7 +160,6 @@ public class FBGraphRequestModule extends ReactContextBaseJavaModule {
                 null,
                 null,
                 null,
-                null,
                 null));
         } else {
             graphRequest.setAccessToken(AccessToken.getCurrentAccessToken());
